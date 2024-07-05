8e2c52dec17545cf7e4c409dba409eaa91fa2d20
cedricmaciel <164974899+cedricmaciel@users.noreply.github.com>
2024-07-05 20:01:12 -0300

    transação de pagina finalizada

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-document.getElementById('pagina-animacao').addEventListener('click', function(event) {
-    event.preventDefault();  // Impede o redirecionamento imediato
 
-    alert('Link "Tabuleiro" clicado');  // Adiciona um alert para verificar o clique
+    document.addEventListener('DOMContentLoaded', function() {
+ 
+        const swordSound = document.getElementById('sword-sound');
 
-    const animationContainer = document.getElementById('animation-container');
-    animationContainer.classList.add('show');
+        document.getElementById('pagina-animacao').addEventListener('click', function(event) {
+            event.preventDefault();  
+            swordSound.play(); 
+            
+           
+            
+            const animationContainer = document.getElementById('animation-container');
+            animationContainer.classList.add('show');
+            
+            setTimeout(() => {
+                window.location.href = 'boardgames.html';  // 
+            }, 4000); 
+        });
+    });
 
-    setTimeout(() => {
-        window.location.href = this.href;  // Redireciona para a página após a animação
-    }, 1000);  // Duração da animação
-});
